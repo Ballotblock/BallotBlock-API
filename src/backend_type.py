d7bd5a5b52c9bb9debf92da2ebfb7d0a7fbc251c
Samuel Vargas <savarga1@asu.edu>
2018-02-17 12:21:07 -0700

    Rename BackendType methods to align with PEP8

--- src/backend_type.py
+++ src/backend_type.py
@@ -8,15 +8,16 @@
 from typing import List
 from enum import Enum, unique
 
+
 @unique
 class BackendType(Enum):
     SQLite = 1
     HyperLedger = 2
 
     @staticmethod
-    def getEnumsAsList() -> List[str]:
+    def get_enums_as_list() -> List[str]:
         return [name for name in BackendType.__members__]
 
     @staticmethod
-    def isValidType(name: str) -> bool:
+    def is_valid_type(name: str) -> bool:
         return name in BackendType.__members__
