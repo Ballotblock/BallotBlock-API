255e8f2719868f102ed95017afefc56fd261a32d
Samuel Vargas <savarga1@asu.edu>
2018-02-17 12:11:46 -0700

    Add backend_type module

---
+++ src/backend_type.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+#
+# src/backend_type.py
+# Authors:
+#     Samuel Vargas
+#
+
+from typing import List
+from enum import Enum, unique
+
+@unique
+class BackendType(Enum):
+    SQLite = 1
+    HyperLedger = 2
+
+    @staticmethod
+    def getEnumsAsList() -> List[str]:
+        return [name for name in BackendType.__members__]
+
+    @staticmethod
+    def isValidType(name: str) -> bool:
+        return name in BackendType.__members__
