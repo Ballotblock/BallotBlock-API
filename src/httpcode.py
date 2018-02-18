bbe96a8a472df6da609750111c071c15f581cbca
Samuel Vargas <savarga1@asu.edu>
2018-02-17 17:09:28 -0700

    Add new status codes to httpcode.py

--- src/httpcode.py
+++ src/httpcode.py
@@ -19,9 +19,21 @@ SIGNUP_OK = \
 ELECTION_CREATED_SUCCESSFULLY = \
     _httpcode("Election successfully created.", status.HTTP_201_CREATED)
 
+LOGIN_SUCCESSFUL = \
+    _httpcode("Login successful. You are now authenticated.", status.HTTP_200_OK)
+
 # Client Errors (4xx)
 MISSING_OR_MALFORMED_JSON = \
     _httpcode("JSON was not provided or is non parseable", status.HTTP_400_BAD_REQUEST)
+
+MISSING_LOGIN_PARAMETERS = \
+    _httpcode("Missing 'username', 'password', or 'account_type'", status.HTTP_400_BAD_REQUEST)
+
+USER_NOT_REGISTERED = \
+    _httpcode("Register with the Registration server prior to signing-in", status.HTTP_400_BAD_REQUEST)
+
+USER_ALREADY_AUTHENTICATED = \
+    _httpcode("You have aleady logged in. Log out first before logging back in.", status.HTTP_400_BAD_REQUEST)
 
 LOG_IN_FIRST = \
     _httpcode("You are not logged in. Login with /api/login/ first", status.HTTP_403_FORBIDDEN)
