b9e6357caafe0082e9f6a5493ed6359a63c2deda
Samuel Vargas <savarga1@asu.edu>
2018-02-28 09:05:41 -0700

    Add missing JsonValidity namedtuple

--- src/interfaces/json_validator.py
+++ src/interfaces/json_validator.py
@@ -3,13 +3,16 @@
 # src/interfaces/json_validator.py
 # Authors:
 #     Samuel Vargas
+
+from typing import Dict
+from typing import NamedTuple
+from collections import namedtuple
 import abc
+
+JsonValidity = namedtuple("jsonvalidty", ("reason", "valid"))
+
 
 class JsonValidator(abc.ABC):
     @abc.abstractmethod
-    def is_valid(self) -> bool:
-        pass
-
-    @abc.abstractmethod
-    def reason(self) -> str:
+    def is_valid(self, json_obj: Dict) -> JsonValidity:
         pass
