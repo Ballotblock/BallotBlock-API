1228e47cc6a3c69a3349649b0148577242989e34
Samuel Vargas <savarga1@asu.edu>
2018-03-17 15:12:18 -0700

    Add extra error code to httpcode.py

--- src/httpcode.py
+++ src/httpcode.py
@@ -12,7 +12,11 @@ from flask_api import status
 HttpCode = NamedTuple("httpcode", [("message", str), ("code", int)])
 _httpcode = namedtuple("httpcode", ("message", "code"))
 
+#
 # Successful (2xx)
+#
+
+# General
 SIGNUP_OK = \
     _httpcode("Signup successfully completed.", status.HTTP_201_CREATED)
 
@@ -22,7 +26,11 @@ ELECTION_CREATED_SUCCESSFULLY = \
 LOGIN_SUCCESSFUL = \
     _httpcode("Login successful. You are now authenticated.", status.HTTP_200_OK)
 
+#
 # Client Errors (4xx)
+#
+
+# General
 MISSING_OR_MALFORMED_JSON = \
     _httpcode("JSON was not provided or is non parseable", status.HTTP_400_BAD_REQUEST)
 
@@ -37,3 +45,8 @@ USER_ALREADY_AUTHENTICATED = \
 
 LOG_IN_FIRST = \
     _httpcode("You are not logged in. Login with /api/login/ first", status.HTTP_403_FORBIDDEN)
+
+
+# Elections
+ELECTION_WITH_TITLE_ALREADY_EXISTS = \
+    _httpcode("You cannot create an election with this title because one already exists", status.HTTP_400_BAD_REQUEST)
