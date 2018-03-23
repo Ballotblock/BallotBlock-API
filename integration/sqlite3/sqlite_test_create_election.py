3a72422383f5e9788c135613f6020a52f254b19f
Samuel Vargas <savarga1@asu.edu>
2018-03-23 12:04:17 -0700

    Finish election creation, retrevial, and validation test

--- integration/sqlite3/sqlite_test_create_election.py
+++ integration/sqlite3/sqlite_test_create_election.py
@@ -6,100 +6,107 @@
 #
 
 from unittest.mock import MagicMock
-from src import httpcode
 import json
-import uuid
 import unittest
+import src.intermediary
 import time
 import datetime
-import src.intermediary
+from src.httpcode import ELECTION_CREATED_SUCCESSFULLY
+from src.key import generate_edsca_keypair
+from src.validator import ElectionJsonValidator
+from src.registration import RegistrationServerProvider
+from src.sqlite import SQLiteBackendIO
+from src.key import is_data_signed_with_public_key
 
 JSON_HEADERS = {"Content-Type": "application/json"}
+PRESERVE_CONTEXT_ON_EXCEPTION = False
 
 
 class CreateElectionTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
-        self.app = src.intermediary.start_test_sqlite(":memory:")
+        # Setup server with correct objects
+        backend_io = SQLiteBackendIO(":memory:")
+        registration_provider = RegistrationServerProvider()
+        election_json_validator = ElectionJsonValidator()
 
-        self.election_creator = {
-            "username": "Sam",
-            "password": "123456",
-            "account_type": "election_creator"
+        election_json_validator.is_valid = MagicMock(return_value=(True, "JSON is ok"))
+        registration_provider.is_user_registered = MagicMock(return_value=True)
+
+        self.app = src.intermediary.start_test_sqlite(
+            backend_io=backend_io,
+            election_json_validator=election_json_validator,
+            registration_provider=registration_provider
+        )
+
+        keys = generate_edsca_keypair()
+
+        start_date = int(time.time())
+        end_date = int((datetime.datetime.fromtimestamp(start_date) + datetime.timedelta(days=1)).timestamp())
+
+        self.master_ballot_json_str = json.dumps({
+            "election_title": "The Favorite Color and Shape Election",
+            "description": "This is an election about your favorite color and shape.",
+            "start_date": start_date,
+            "end_date": end_date,
+            "questions": [["Favorite Color?", ["Red", "Blue"]],
+                          ["Favorite Shape?", ["Square", "Triangle"]]]
+        })
+
+        self.election_post = {
+            "master_ballot": self.master_ballot_json_str,
+            "creator_public_key": str(keys.public.to_string().hex()),
+            "master_ballot_signature": str(keys.private.sign(self.master_ballot_json_str.encode('utf-8')).hex())
         }
 
-        now = int(time.time())
-        ten_days_later = int((datetime.datetime.fromtimestamp(now) + datetime.timedelta(days=10)).timestamp())
+        # Session management information
+        self.username = "Samulus"
+        self.account_type = "ElectionType"
 
-        self.election_data = {
-            "username": "Sam",
-            "start_date": now,
-            "end_date": ten_days_later,
-            "title": "Color Shape Election",
-            "description": "This is a test election about favorite color / shape.",
-            "propositions": [
-                {
-                    "question": "What is your favorite color?",
-                    "choices": ["Red", "Green", "Blue", "Yellow"],
-                },
-                {
-                    "question": "What is your favorite shape?",
-                    "choices": ["Triangle", "Square", "Circle", "Diamond"],
-                }
-            ]
-        }
+    def test_a_election_creator_can_create_election(self):
+        with self.app as c:
+            # Mock out logging into the API
+            with c.session_transaction() as sess:
+                sess['username'] = self.username
+                sess['account_type'] = self.account_type
 
-        self.sessionID = uuid.uuid4()
+            response = self.app.post("/api/election/create", headers=JSON_HEADERS, data=json.dumps(self.election_post))
 
-    def test_user_can_create_election(self):
-        # Assume the user is registered.
-        src.intermediary.REGISTRATION_PROVIDER.is_user_registered = MagicMock(return_value=True)
+            assert response.data.decode('utf-8') == ELECTION_CREATED_SUCCESSFULLY.message
+            assert response.status_code == ELECTION_CREATED_SUCCESSFULLY.code
 
-        # Assume the user election data is always valid for now.
-        src.intermediary.ELECTION_JSON_VALIDATOR.is_valid = MagicMock(
-            return_value=(True, "Testing, data is assumed to be valid."))
+    def test_b_election_creator_can_retrieve_and_verify_their_election(self):
+        with self.app as c:
+            # Mock out logging into the API
+            with c.session_transaction() as sess:
+                sess['username'] = self.username
+                sess['account_type'] = self.account_type
 
-        # Log the election creator in
-        response = self.app.post("/api/login",
-                                 headers=JSON_HEADERS,
-                                 data=json.dumps(self.election_creator))
+            # Send get request to server
+            master_ballot_dict = json.loads(self.master_ballot_json_str)
+            data = {"election_title": master_ballot_dict["election_title"]}
 
-        expectedCode = httpcode.LOGIN_SUCCESSFUL.code
-        expectedMessage = httpcode.LOGIN_SUCCESSFUL.message
-        actualCode = response.status_code
-        actualMessage = response.data.decode("utf-8")
-        assert expectedCode == actualCode and actualMessage == expectedMessage, \
-            "Login should have been successful but failed."
+            response = self.app.get("/api/election/get_by_title", headers=JSON_HEADERS, data=json.dumps(data))
+            assert response.status_code == 200
+            retrieved_election = json.loads(response.data)
 
-        # Verify we're now authenticated with the intermediary server
-        response = self.app.post("/api/login",
-                                 headers=JSON_HEADERS,
-                                 data=json.dumps(self.election_creator))
-        expectedCode = httpcode.USER_ALREADY_AUTHENTICATED.code
-        expectedMessage = httpcode.USER_ALREADY_AUTHENTICATED.message
-        actualCode = response.status_code
-        actualMessage = response.data.decode("utf-8")
-        assert expectedCode == actualCode and actualMessage == expectedMessage, \
-            "Intermediary server should see our previous login."
+            # Verify that the server didn't leak the 'election_private_key' as the election
+            # hasn't ended yet. (It ends in 1 day)
+            assert 'election_private_key' not in retrieved_election
 
-        # Send a request to create an election
-        response = self.app.post("/api/election/create",
-                                 headers=JSON_HEADERS,
-                                 data=json.dumps(self.election_data))
+            # Verify that the election data used to create the election is identical
+            # to the data that was retrieved.
+            assert retrieved_election["election_title"] == master_ballot_dict["election_title"]
+            assert retrieved_election["description"] == master_ballot_dict["description"]
+            assert retrieved_election["start_date"] == master_ballot_dict["start_date"]
+            assert retrieved_election["end_date"] == master_ballot_dict["end_date"]
+            assert json.loads(retrieved_election["questions"]) == master_ballot_dict["questions"]
+            assert retrieved_election["creator_username"] == self.username
+            assert retrieved_election["creator_public_key"] == self.election_post["creator_public_key"]
+            assert retrieved_election["master_ballot_signature"] == self.election_post["master_ballot_signature"]
 
-        # TODO:
-        # * Implement election_create()
-        # * Implement election
-
-        # 4) Verify that when we query the server with our username
-        # 5) We recieve
-
-        # Tasks to complete for this integration test:
-        # 1) Send request to server to create an election with user_a
-        # 2) user_b joins the election, receives a set of public private keys
-        # 3) user_b downloads a ballot schema for this particular election
-        # 4) user_b fills out the ballot schema with their voting choices
-        # 5) user_b casts their ballot cementing it in the voting ledger / db
-        # 6) user_b redownloads the blockchain / db and manually verifies that their
-        #    encrypted ballot was included.
+            # Verify that the server did not tamper with our data by validating our signature
+            assert is_data_signed_with_public_key(retrieved_election["creator_public_key"],
+                                                  retrieved_election["master_ballot_signature"],
+                                                  self.master_ballot_json_str)
