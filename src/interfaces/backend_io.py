0106e24a25e16a2d6120d15b8df4c2e6b2ab68ca
Samuel Vargas <savarga1@asu.edu>
2018-03-23 12:01:57 -0700

    Remove oudated comments about interface API
    It's better to have no comments than misleading or outdated ones. I'll replace these with an up-to-date version soon.

--- src/interfaces/backend_io.py
+++ src/interfaces/backend_io.py
@@ -10,76 +10,25 @@ import abc
 
 class BackendIO(abc.ABC):
     @abc.abstractmethod
-    def create_election(self, election_details: Dict):
-        """
-        Receives a dictionary with the following schema:
-        :param election_details:
-        {
-            "election_title": "The Favorite Color and Shape Election",
-            "description": "This is the description of this Election",
-            "start_date": 1380238023 (unix timestamp),
-            "end_date": 32808320 (unix timestamp),
-            "creator_username": "Username",
-            "questions:" "[
-                ["Favorite Color?", ["Red", Blue]],
-                ["Favorite Shape?", ["Square", "Triangle]]
-            ]",
-            "public_key": "Public key for the entire election",
-            "private_key": "Private key for the entire election,
-        }
-        """
+    def create_election(self, master_ballot: Dict,
+                        creator_master_ballot_signature: str = None,
+                        creator_public_key_hex: str = None,
+                        backend_public_key_hex: str = None,
+                        backend_private_key_hex: str = None):
         raise NotImplementedError
 
     @abc.abstractmethod
     def create_ballot(self, ballot: Dict):
-        """
-        Receives a dictionary with the following schema:
-
-        :param ballot:
-        {
-            "voter_id" "uuid4",
-            "election_title": "The Favorite Color and Shape Election",
-            "answers": "['Red', 'Triangle']"
-        }
-
-        :return:
-        """
         raise NotImplementedError
 
     @abc.abstractmethod
     def get_election_by_title(self, election_title: str) -> Optional[Dict]:
-        """
-        Should return a dictionary from the backend with the following schema:
-
-        :param election_title The title of the election.
-
-        :return:
-        {
-            "election_title": "The Favorite Color and Shape Election",
-            "description": "This is the description of this Election",
-            "start_date": 1380238023 (unix timestamp),
-            "end_date": 32808320 (unix timestamp),
-            "creator_username": "Username",
-            "questions:" "[
-                ["Favorite Color?", ["Red", Blue]],
-                ["Favorite Shape?", ["Square", "Triangle]]
-            ]",
-            "public_key": "Public key for the entire election",
-            "private_key": "Private key for the entire election,
-        }
-        """
         raise NotImplementedError
 
     @abc.abstractmethod
     def get_ballot_by_id(self, voter_id: str) -> Optional[Dict]:
-        """
-        Should return a dictionary with the following schema:
-        :param voter_id: The voter ID that corresponds to a specific ballot, should be a uuid4
-        :return:
-        {
-            "voter_id" "uuid4",
-            "election_title": "The Favorite Color and Shape Election",
-            "answers": "['Red', 'Triangle']"
-        }
-        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def has_user_participated_in_election(self, username: str, election_title: str) -> bool:
         raise NotImplementedError
