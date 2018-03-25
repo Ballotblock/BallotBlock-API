c5eedc1ef936f396c023af9584ceeb2041d9dfcd
Samuel Vargas <savarga1@asu.edu>
2018-03-24 18:41:15 -0700

    Add ECDSAKeyPair class to crypto_suite.py

--- src/crypto_suite.py
+++ src/crypto_suite.py
@@ -13,6 +13,29 @@ ECDSA_CURVE = ecdsa.SECP256k1
 ECDSA_256k1_KeyPair = NamedTuple("ECDSA_256k1_KeyPair", [("public", VerifyingKey), ("private", SigningKey)])
 
 
+class ECDSAKeyPair:
+    def __init__(self):
+        self.__private = SigningKey.generate(curve=ECDSA_CURVE)
+        self.__public = self.__private.get_verifying_key()
+
+    def get_public_key(self) -> VerifyingKey:
+        return self.__public
+
+    def get_private_key(self) -> SigningKey:
+        return self.__private
+
+    def get_public_key_hex_str(self) -> str:
+        return str(self.__public.to_string().hex())
+
+    def sign_with_private_key_and_retrieve_hex_signature(self, plaintext: str) -> str:
+        return str(self.__private.sign(plaintext.encode('utf-8')).hex())
+
+    def is_signed_with_private_key(self, signature_hex: str, plaintext):
+        return self.__public.verify(bytes.fromhex(signature_hex), bytes(plaintext.encode('utf-8')))
+
+
+
+# TODO: Remove this class
 class CryptoSuite:
     @staticmethod
     def generate_key_pair() -> ECDSA_256k1_KeyPair:
