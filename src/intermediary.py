606d88595ffb980634d5916dfc05190b18b81e7b
Samuel Vargas <savarga1@asu.edu>
2018-03-26 20:38:41 -0700

    Add ability to cast a vote from intermediary.py

--- src/intermediary.py
+++ src/intermediary.py
@@ -33,10 +33,12 @@ from src.sessions import SessionManager
 from src.registration import RegistrationServerProvider
 from src.interfaces import BackendIO
 from src import required_keys
-from src.ecdsa_keypair import ECDSAKeyPair
+from src.crypto_suite import ECDSAKeyPair, FernetCrypt, RSAKeyPair
+from src.crypto_flow import verify_data_is_signed_ecdsa
 import json
 import uuid
 import time
+import base64
 
 app = Flask(__name__)
 
@@ -57,6 +59,7 @@ app.config['PRESERVE_CONTEXT_ON_EXCEPTION'] = False
 
 
 def start_test_sqlite(backend_io: BackendIO = None,
+                      session_manager=SessionManager(),
                       registration_provider=RegistrationServerProvider(),
                       election_json_validator=ElectionJsonValidator()
                       ):
@@ -64,10 +67,12 @@ def start_test_sqlite(backend_io: BackendIO = None,
         raise ValueError("backend_io can't be none")
 
     global BACKEND_IO
+    global SESSION_MANAGER
     global REGISTRATION_PROVIDER
     global ELECTION_JSON_VALIDATOR
 
     BACKEND_IO = backend_io
+    SESSION_MANAGER = session_manager
     REGISTRATION_PROVIDER = registration_provider
     ELECTION_JSON_VALIDATOR = election_json_validator
 
@@ -157,29 +162,34 @@ def election_create() -> httpcode.HttpCode:
     if BACKEND_IO.get_election_by_title(master_ballot["election_title"]) is not None:
         return httpcode.ELECTION_WITH_TITLE_ALREADY_EXISTS
 
-    # Verify that we can decrypt the "signature" using "ballot" + "public_key"
-    if not ECDSAKeyPair.is_data_signed(content['creator_public_key'],
-                                       content['master_ballot_signature'],
-                                       content['master_ballot']):
+    # Verify that the election creator correctly signed their 'master_ballot_signature' using
+    # their 'creator_public_key'
+    if not verify_data_is_signed_ecdsa(
+            content['master_ballot'],
+            content['master_ballot_signature'],
+            content['creator_public_key']
+    ):
         return httpcode.ELECTION_BALLOT_SIGNING_MISMATCH
 
-    # Generate a public private key pair, used to encrypt voter ballots.
-    election_keys = ECDSAKeyPair()
+    # Election Encryption Workflow:
+    # 1) Generate an RSAKeyPair
+    # 2) Generate a FernetObject (each contains a random symmetric key)
+    # 3) Encrypt the random symmetric key using the RSAKeyPair (public key)
+    # 4) Stick the private key, public key, and encrypted fernet key into the database
 
-    # Create the election using the specified:
-    #   [*] master_ballot,
-    #   [*] creator signature + public key,
-    #   [*] per election public private key
+    election_rsa = RSAKeyPair()
+    election_fernet = FernetCrypt()
+    election_encrypted_fernet_key = election_rsa.encrypt_message_as_b64(election_fernet.get_key_as_bytes())
 
     master_ballot['questions'] = json.dumps(master_ballot['questions'])
-
     BACKEND_IO.create_election(
         master_ballot,
         creator_username=SESSION_MANAGER.get_username(session),
         creator_master_ballot_signature=content['master_ballot_signature'],
-        creator_public_key_hex=content['creator_public_key'],
-        election_public_key_hex=election_keys.get_public_key_hex_str(),
-        election_private_key_hex=election_keys.get_private_key_hex_str()
+        creator_public_key_b64=content['creator_public_key'],
+        election_private_rsa_key=election_rsa.get_private_key_as_pkcs1_b64(),
+        election_public_rsa_key=election_rsa.get_public_key_as_pkcs1_b64(),
+        election_encrypted_fernet_key=election_encrypted_fernet_key
     )
 
     return httpcode.ELECTION_CREATED_SUCCESSFULLY
@@ -215,3 +225,74 @@ def election_get_by_title():
         result.pop('election_private_key')
 
     return jsonify(result), 200
+
+
+@app.route("/api/election/vote", methods=["GET", "POST"])
+def election_cast_vote():
+    # Check if anyone is logged in
+    if not SESSION_MANAGER.is_logged_in(session):
+        return httpcode.LOG_IN_FIRST
+
+    # Check if any JSON was supplied at all
+    content = request.get_json(silent=True, force=True)
+    if content is None:
+        return httpcode.MISSING_OR_MALFORMED_JSON
+
+    # Verify that 'public_key', 'ballot', and 'ballot_signature' were provided
+    for key in required_keys.REQUIRED_ELECTION_VOTE_KEYS:
+        if key not in content:
+            return httpcode.ELECTION_VOTER_BALLOT_MISSING_BALLOT_PUBLIC_KEY_OR_SIGNATURE
+
+    # Verify that 'ballot' is valid JSON
+    try:
+        ballot = json.loads(content['ballot'])
+    except ValueError:
+        return httpcode.ELECTION_VOTER_BALLOT_JSON_IS_MALFORMED
+
+    # Verify that the 'election_title' and 'answers' were provided in the ballot
+    for key in required_keys.REQUIRED_ELECTION_VOTE_BALLOT_KEYS:
+        if key not in ballot:
+            return httpcode.ELECTION_VOTER_BALLOT_MISSING_BALLOT_PUBLIC_KEY_OR_SIGNATURE
+
+    # Verify that this election actually exists
+    election = BACKEND_IO.get_election_by_title(ballot['election_title'])
+    if election is None:
+        return httpcode.ELECTION_NOT_FOUND
+
+    # TODO: Verify that the provided answers match the question options!
+    # TODO: Verify that the election hasn't ended already
+
+    # Verify that the user signed their ballot data correctly
+    if not verify_data_is_signed_ecdsa(
+            content['ballot'],
+            content['ballot_signature'],
+            content['voter_public_key']
+    ):
+        return httpcode.ELECTION_BALLOT_SIGNING_MISMATCH
+
+    # Fetch the RSA Key from the ballot's corresponding election
+    election_rsa = RSAKeyPair(
+        use_public_pkcs1_b64_key=election["election_public_key"].decode('utf-8'),
+        use_private_pkcs1_b64_key=election["election_private_key"].decode('utf-8')
+    )
+
+    # Decrypt the fernet symmetric key
+    decrypted_fernet_key = election_rsa.decrypt_b64_to_bytes(election["election_encrypted_fernet_key"])
+    fernet_crypt = FernetCrypt(
+        use_fernet_key_bytes=decrypted_fernet_key
+    )
+
+    # Encrypt the user's ballot using our decrypted symmetric fernet key
+    encrypted_ballot = fernet_crypt.encrypt_to_b64(content["ballot"].encode('utf-8')).decode('utf-8')
+
+    voter_uuid = str(uuid.uuid4())
+
+    BACKEND_IO.create_ballot(
+        encrypted_ballot,
+        election_title = election['election_title'],
+        voter_uuid=voter_uuid,
+        ballot_signature=content['ballot_signature'],
+        voter_public_key_b64=content['voter_public_key'],
+    )
+
+    return str(voter_uuid), 201
