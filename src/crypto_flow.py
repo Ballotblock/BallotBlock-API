88fe57dfc5030a5b39fcaa63a96dc2910df2701d
Samuel Vargas <savarga1@asu.edu>
2018-03-26 20:37:50 -0700

    Add 'crypto_flow' module for high level operations

--- src/crypto_flow.py
+++ src/crypto_flow.py
@@ -7,16 +7,16 @@
 
 import base64
 from ecdsa import SigningKey, VerifyingKey, BadSignatureError
-from src.crypto_suite import ECDSAKeyPair
+from src.crypto_suite import ECDSAKeyPair, ECDSA_CURVE
 
 
 def verify_data_is_signed_ecdsa(
         data: bytes = None,
         string_signature_b64: bytes = None,
         user_public_key_ecdsa_b64: bytes = None) -> bool:
-    public_key = VerifyingKey().from_string(base64.b64decode(user_public_key_ecdsa_b64))
+    public_key = VerifyingKey.from_string(base64.b64decode(user_public_key_ecdsa_b64), curve=ECDSA_CURVE)
     try:
-        result = public_key.verify(base64.b64decode(string_signature_b64), data)
+        public_key.verify(base64.b64decode(string_signature_b64), data.encode('utf-8'))
     except BadSignatureError:
         return False
 
