452709156bf46f6e99423b1d4c5024e92ad835d9
Samuel Vargas <savarga1@asu.edu>
2018-03-30 17:10:41 -0700

    Add comments to backend_io

--- src/interfaces/backend_io.py
+++ src/interfaces/backend_io.py
@@ -13,17 +13,65 @@ class BackendIO(abc.ABC):
     @abc.abstractmethod
     def create_election(self, master_ballot: Dict = None,
                         creator_username: str = None,
-                        creator_master_ballot_signature: str = None,
-                        creator_public_key_b64: str = None,
-                        election_private_rsa_key: str = None,
-                        election_public_rsa_key: str = None,
-                        election_encrypted_fernet_key: str = None):
+                        creator_master_ballot_signature_b64: str = None,
+                        creator_public_ecdsa_key_b64: str = None,
+                        election_private_rsa_key_b64: str = None,
+                        election_public_rsa_key_b64: str = None,
+                        election_encrypted_fernet_key_b64: str = None):
+        """
+        Create an election on the backend:
+
+        The "election_title" inside of the master_ballot dictionary is used
+        to uniquely identify any given election.
+
+        All of the following data should be stored
+
+
+        :param master_ballot: A dictionary with the following format:
+                              {
+                                  "description": "Pick your favorite color / shape.",
+                                  "election_title": "Favorite Color and Shape Election",
+                                  "start_date": 1521507388,
+                                  "end_date": 1522371388,
+                                  "questions": [
+                                      ["Favorite Color?", ["Red", "Blue"]],
+                                      ["Favorite Shape?", ["Square", "Triangle"]]
+                                   ]
+                              }
+
+        :param creator_username: The individual who created this election.
+                                 The creator_username should be associated with the election.
+
+        :param creator_master_ballot_signature_b64: It's the signed version of the "master_ballot" dictionary
+                                                    in string format
+
+        :param creator_public_ecdsa_key_b64: The creator's public ECDSA key in base64 format.
+
+        :param election_private_rsa_key: The election's private RSA key in base64 format.
+
+        :param election_public_rsa_key: The election's public RSA key in base64 format.
+
+        :param election_encrypted_fernet_key_b64: The election's encrypted symmetric fernet key in base64 format.
+
+
+        """
         raise NotImplementedError
 
     @abc.abstractmethod
     def create_ballot(self, ballot: str,
-                            ballot_signature: str = None,
+                            ballot_signature_b64: str = None,
                             voter_public_key_b64: str = None):
+        """
+        :param ballot: A dictionary with the following format
+                       {
+                        'election_title': 'The Favorite Color and Shape Election',
+                        'answers': '['Red', 'Triangle']'
+                       }
+
+        :param ballot_signature_b64: A base64 encoded ballot signature
+        :param voter_public_key_b64: A base64 encoded voter_public_key_b64
+        :return:
+        """
         raise NotImplementedError
 
     @abc.abstractmethod
