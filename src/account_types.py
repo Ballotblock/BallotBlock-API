1f412d7bc8a74e090b694693b40b97f1fc4adf4b
Samuel Vargas <savarga1@asu.edu>
2018-04-04 14:54:37 -0700

    Remove unused methods from AccountTypes.py

--- src/account_types.py
+++ src/account_types.py
@@ -12,11 +12,3 @@ from enum import Enum, unique
 class AccountType(Enum):
     voter = 1
     election_creator = 2
-
-    @staticmethod
-    def getEnumsAsList() -> List[str]:
-        return [name for name in AccountType.__members__]
-
-    @staticmethod
-    def isValidType(name: str) -> bool:
-        return name in AccountType.__members__
