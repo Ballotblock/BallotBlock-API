94cf1c9931c89f5dd1f2686303338cb459b8fe00
Samuel Vargas <savarga1@asu.edu>
2018-04-04 11:08:14 -0700

    Add correct try catch signatures to authentication.py

--- src/authentication_cookie.py
+++ src/authentication_cookie.py
@@ -5,27 +5,48 @@
 #     Alex Gao
 
 # https://github.com/pyca/cryptography/issues/1333#issuecomment-55481324
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from cryptography.hazmat.backends import default_backend
-from cryptography.fernet import Fernet, InvalidToken
+from src.cookie_encryptor import CookieEncryptor
 from src.account_types import AccountType
-from flask import request
+from typing import Optional
+from json import JSONDecodeError
 import json
-import os
-import base64
 
 class AuthenticationCookie:
     @staticmethod
-    def is_encrypted_by_registration_server(cookies) -> bool:
+    def is_encrypted_by_registration_server(password: str, cookies) -> bool:
+        try:
+            token = json.loads(cookies.get("token"))
+        except JSONDecodeError:
+            return False
+
+        if 'authentication' not in token:
+            return False
+
+        if CookieEncryptor(password).decrypt(token['authentication'].encode('utf-8')) is None:
+            return False
+
+        return True
 
     @staticmethod
-    def get_username(cookies) -> str:
-        token = json.loads(cookies.get("token"))
-        return token['username']
+    def get_username(cookies) -> Optional[str]:
+        try:
+            token = json.loads(cookies.get("token"))
+        except JSONDecodeError:
+            return None
+
+        return token.get('username')
 
     @staticmethod
-    def get_account_type(cookies) -> str
-        token = json.loads(cookies.get("token"))
-        return token['account_type']
-        return AccountType(token['account_type'])
+    def get_account_type(cookies) -> Optional[AccountType]:
+        try:
+            token = json.loads(cookies.get("token"))
+        except JSONDecodeError:
+            return None
+
+        if 'account_type' not in token:
+            return None
+
+        try:
+            return AccountType(token['account_type'])
+        except ValueError:
+            return None
